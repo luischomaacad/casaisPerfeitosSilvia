f70fada6fd57b59c496652c6ec41eff66eec3313
Mathias Voelcker <mathias_v94@Mathiass-MacBook-Pro.local>
2018-04-29 16:04:44 -0300

    criar leitor de arquivo e configuracao inicial

--- src/App.java
+++ src/App.java
@@ -1,8 +1,20 @@
+import sun.security.krb5.Config;
+
+import java.io.IOException;
 
 public class App {
 	
 	public static void main(String[] args) {
-		Map map = new Map();
+        ConfigIniciais configIniciais = new ConfigIniciais();
+		try {
+			configIniciais = Leitor.lerArquivo("teste.txt");
+		} catch (NotTxtException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        Map map = new Map();
 		map.initialize();
 		map.printMap();
 	}
---
+++ src/Leitor.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Leitor {
+
+
+    public static ConfigIniciais lerArquivo(String nameArq) throws NotTxtException, IOException {
+        String fileString = "";
+        Scanner scanner;
+        ConfigIniciais configIniciais = new ConfigIniciais();
+        if (!nameArq.contains(".txt")) {
+            throw new NotTxtException("Arquivo deve ser txt");
+        }
+        final Reader reader = new FileReader(nameArq);
+        final BufferedReader bufferReader = new BufferedReader(reader);
+        String currentLine;
+        currentLine = bufferReader.readLine();
+        scanner = new Scanner(currentLine);
+        int numCasais = scanner.nextInt();
+        configIniciais.setNumCasais(numCasais);
+        configIniciais.setNumCartorios(scanner.nextInt());
+        popularCandidatos(Genero.MASCULINO, configIniciais, bufferReader);
+        popularCandidatos(Genero.FEMININO, configIniciais, bufferReader);
+        reader.close();
+        bufferReader.close();
+        return configIniciais;
+    }
+
+
+    private static void popularCandidatos(Genero genero, ConfigIniciais configIniciais, BufferedReader bf) throws IOException {
+        for(int i = 0; i < configIniciais.getNumCasais(); i++){
+            Candidato candidato = new Candidato();
+            String currentLine = bf.readLine();
+            Scanner scanner = new Scanner(currentLine);
+            candidato.setId(scanner.nextInt());
+            candidato.setGenero(genero);
+            while(scanner.hasNextInt()){
+                candidato.adicionarPreferencias(scanner.nextInt());
+            }
+            configIniciais.adicionarCandidato(candidato);
+        }
+    }
+}
